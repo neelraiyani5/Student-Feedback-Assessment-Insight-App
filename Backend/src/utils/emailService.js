825b3b954ac638ff261276e3a673d35406b3123e
neelraiyani5 <neelraiyani5@gmail.com>
2026-02-13 16:10:52 +0530

    email service fix

--- Backend/src/utils/emailService.js
+++ Backend/src/utils/emailService.js
@@ -8,11 +8,14 @@ const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: Number(process.env.SMTP_PORT) === 465,
-    family: 4, 
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 });
 
 
