08afd5e5bc9672c940e39628307788b9a86cc673
neelraiyani5 <neelraiyani5@gmail.com>
2026-02-13 15:53:39 +0530

    Email service fix

--- Backend/src/utils/emailService.js
+++ Backend/src/utils/emailService.js
@@ -6,13 +6,15 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
+    port: Number(process.env.SMTP_PORT),
+    secure: Number(process.env.SMTP_PORT) === 465,
+    family: 4, 
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
 });
+
 
 export const sendCredentials = async (email, name, userId, password) => {
     const mailOptions = {
